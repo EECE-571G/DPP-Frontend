f0bbb049e33170172f959d022a60543a1256d83d
guorunhe <56808163+guorunhe-design@users.noreply.github.com>
2025-04-16 16:07:10 -0700

    update address

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,12 +3,12 @@ import { ethers } from 'ethers';
 
 const poolManagerRaw = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const positionManagerRaw = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const desiredPricePoolHookRaw = "0xb6a68881d3Ee8FB71CafEe3CBB1A1a8747fd2aC4";
+const desiredPricePoolHookRaw = "0x26F1cd0223Cd26F9303D8414B01B81541ca22Ac4";
 const desiredPricePoolHelperRaw = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 const token0Raw = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
 const token1Raw = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
 const token2Raw = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
-const governanceTokenRaw = "0x08aEd9A0e715b4D37BBF25E8E1fE6bd89e93BF0e";
+const governanceTokenRaw = "0xf1df33493e79c6df4813b81891799fFA53fB42f6";
 const governanceContractRaw = desiredPricePoolHookRaw;
 
 // Export checksummed addresses
